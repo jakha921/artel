5ba782dc11679b86f4278b1e0b872f00d1dd67c4
Jakha921 <jakha921@gmail.com>
2021-12-07 19:27:45 +0500

    clean repo

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn artel.wsgi --log-file -
--- artelApp/permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import permissions
-
-class IsAuthenticatedOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
